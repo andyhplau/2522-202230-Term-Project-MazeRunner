60fe3e13e35e80a72f3bdbb7d6bc93c776c283ee
Soohyeun <soohyeunp@gmail.com>
2022-11-20 00:16:40 -0800

    Create character class

--- src/main/java/ca/bcit/comp2522/termproject/mazerunner/model/Character.java
+++ src/main/java/ca/bcit/comp2522/termproject/mazerunner/model/Character.java
@@ -1,30 +1,74 @@
 package ca.bcit.comp2522.termproject.mazerunner.model;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
+import javafx.scene.Group;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
-import javafx.scene.shape.Rectangle;
-import javafx.stage.Stage;
 
-import java.io.IOException;
+/**
+ * Create character user will use.
+ *
+ * @author Andy & Soo
+ * @version 2022
+ */
+public class Character extends Group {
+    private final int offsetX = 0;
+    private final int offsetY = 0;
+    private final int width = 50;
+    private final int height = 50;
+    private final int appWidthHeight = 600;
 
-public abstract class Character extends Pane {
-
-    // Character Configuration
-    ImageView imageView;
-
-    int offsetX = 0;
-    int offsetY = 0;
-    int width = 32;
-    int height = 32;
-
-    public Character(ImageView imageView) {
-        this.imageView = imageView;
+    /**
+     * Constructs the character with passed image.
+     *
+     * @param imageView image file user will use
+     */
+    public Character(final ImageView imageView) {
+        imageView.setX(offsetX);
+        imageView.setY(offsetY);
+        imageView.setFitHeight(height);
+        imageView.setFitWidth(width);
 
         getChildren().addAll(imageView);
     }
 
+    // Todo: check wall in the map inside the if-statement!
+    /**
+     * Modifies the x-position of the image.
+     *
+     * @param x number of movement of x coordinate
+     */
+    public void moveX(final int x) {
+        boolean right = x > 0;
+        for (int i = 0; i < Math.abs(x); i++) {
+            if (right) {
+                if (getTranslateX() < appWidthHeight - offsetX - width) {
+                    this.setTranslateX(this.getTranslateX() + 1);
+                }
+            } else {
+                if (getTranslateX() > 0) {
+                    this.setTranslateX(this.getTranslateX() - 1);
+                }
+            }
+        }
+    }
 
+    // Todo: check wall in the map inside the if-statement!
+    /**
+     * Modifies the y-position of the image.
+     *
+     * @param y number of movement of y coordinate
+     */
+    public void moveY(final int y) {
+        boolean right = y > 0;
+        for (int i = 0; i < Math.abs(y); i++) {
+            if (right) {
+                if (getTranslateY() < appWidthHeight - offsetY - height) {
+                    this.setTranslateY(this.getTranslateY() + 1);
+                }
+            } else {
+                if (getTranslateY() > 0) {
+                    this.setTranslateY(this.getTranslateY() - 1);
+                }
+            }
+        }
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,12 @@
 module ca.bcit.comp2522.termproject.mazerunner {
     requires javafx.controls;
+    requires javafx.graphics;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens ca.bcit.comp2522.termproject.mazerunner to javafx.fxml;
     exports ca.bcit.comp2522.termproject.mazerunner;
     exports ca.bcit.comp2522.termproject.mazerunner.model;
-    opens ca.bcit.comp2522.termproject.mazerunner.model to javafx.fxml;
+    exports ca.bcit.comp2522.termproject.mazerunner.view;
 }
