aee6ffebe9827c4a02f7e9eb9fb261714772ea71
Soohyeun <soohyeunp@gmail.com>
2022-12-04 22:31:49 -0800

    Activate exit button

--- src/main/java/ca/bcit/comp2522/termproject/mazerunner/MazeRunnerDriver.java
+++ src/main/java/ca/bcit/comp2522/termproject/mazerunner/MazeRunnerDriver.java
@@ -58,6 +58,9 @@ public class MazeRunnerDriver extends Application {
         Button exit = new Button("Exit");
         exit.setFont(BUTTON_FONT_SIZE);
         exit.setMinWidth(buttonWidth);
+        exit.setOnAction(actionEvent -> {
+            stage.close();
+        });
 
         VBox vBox = new VBox(spacing);
         vBox.getChildren().addAll(welcome, instruction, play, pastHistory, exit);
